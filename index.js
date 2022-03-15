a1075c9b44d25ccc2785b3fb60941509a55ab22d
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-15 10:40:57 -0400

    commit tree 2e7629a9523914326bad9468b47e44b73c0875fe

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+if (distance > 2500) {
+  return 'No can do.';
+}else if (distance > 2000) {
+  return 'I will gladly take your thirty bucks.';
+}else if (distance <= 400) {
+  return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+// Write your code here!
+
+const a = (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+return a;
+} 
+
+
+
+function switchOnCharmFromTip(tip){
+// Write your code here!
+switch(tip) {
+  case 'generous': 
+  return 'Thank you so much.';
+  break;
+  case 'not as generous':
+    return 'Thank you.';
+  break;
+  default:
+    return 'Bye.';
+  break;
 }
+}
+
